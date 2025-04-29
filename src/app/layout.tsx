72317f48cbb0f8bcdb739eb21f7691aa6a28283b
Lawrence Ma <lawrencema415@gmail.com>
2025-04-29 15:08:39 -0700

    Fix missing styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang='en'>
-			<body>
+		<html lang='en' className='dark'>
+			<body className='min-h-screen bg-background text-foreground'>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
