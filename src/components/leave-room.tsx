29fb9cac59725de22ff16b5f9135fed04ba38beb
Lawrence Ma <lawrencema415@gmail.com>
2025-05-05 03:27:52 -0700

    Fix vercel build fail

--- src/components/leave-room.tsx
+++ src/components/leave-room.tsx
@@ -4,9 +4,10 @@ import { useGame } from '@/hooks/use-game';
 export default function LeaveRoomButton() {
 	const { leaveRoomMutation } = useGame();
 
+	// TODO: FIX handleLeaveRoom/route
 	const handleLeaveRoom = () => {
 		console.log('Leave Room button clicked');
-		leaveRoomMutation.mutate();
+		leaveRoomMutation.mutate('temp');
 	};
 
 	return (
