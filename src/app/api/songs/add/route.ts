e0b8bb3fb0c62c9d489b814c63bd76872bab3967
dazedgit <103487062+dazedgit@users.noreply.github.com>
2025-05-07 16:25:15 -0700

    implemented add and remove song server routes

---
+++ src/app/api/songs/add/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import { verifyAuthInRouteHandler } from '@/lib/auth';
+import { storage } from '../../storage';
+import { broadcastGameState } from '@/lib/broadcast';
+
+export async function POST(req: Request) {
+  try {
+    const { user, response } = await verifyAuthInRouteHandler();
+    
+    if (!user) {
+      return response;
+    }
+
+    const body = await req.json();
+    const { roomCode, song } = body;
+
+    if (!roomCode ) {
+      return NextResponse.json(
+        { error: 'Room not found' },
+        { status: 400 }
+      );
+    }
+
+    // Find room by code using Redis storage
+    const roomId = await storage.getRoomByCode(roomCode);
+    if (!roomId) {
+      return NextResponse.json({ error: 'Room not found' }, { status: 404 });
+    }
+
+    // Get the current game state
+    const gameState = await storage.getGameStateByRoomCode(roomCode);
+    if (!gameState) {
+      return NextResponse.json({ error: 'Game state not found' }, { status: 404 });
+    }
+    
+    await storage.addSongToRoom(roomCode, song)
+    await broadcastGameState(roomCode, storage);
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    return NextResponse.json(
+      { error: 'Failed to add song' },
+      { status: 500 }
+    );
+  }
+}
